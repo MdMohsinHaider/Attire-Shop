b4bed9a2921db68235b1895816de69bada7671b9
Mohsin <mdmohsinhaider6@gmail.com>
2024-12-03 21:04:03 +0530

    ShirtDao

--- src/com/attireshop/dao/ShirtDao.java
+++ src/com/attireshop/dao/ShirtDao.java
@@ -1,12 +1,9 @@
 package com.attireshop.dao;
 
 public class ShirtDao {
+
     // Insert Shirt
     public void saveShirtDao(){
         //
     }
-    // Delete Shirt.
-    // Update Shirt Property.
-    // Display all the Shirts.
-    // Display Shirt By I'd.
 }
